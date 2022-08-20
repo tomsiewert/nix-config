0db9b7579b75cc430326097d755cabb8836dbfa8
Tom Siewert <tom@siewert.io>
2022-08-20 20:43:51 +0200

    flake: Add sops
    Signed-off-by: Tom Siewert <tom@siewert.io>

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    agenix.url = "github:ryantm/agenix";
+    sops-nix.url = "github:Mic92/sops-nix";
 
     home-manager = {
       url = "github:nix-community/home-manager/release-22.05";
@@ -12,7 +12,7 @@
     };
   };
 
-  outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, agenix, home-manager }:
+  outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, sops-nix, home-manager }:
     let
       overlay-unstable = final: prev: {
         unstable = import inputs.nixpkgs-unstable { config.allowUnfree = true; system = final.system; };
@@ -24,6 +24,7 @@
           modules = [
             ({ config, pkgs, ...}: { nixpkgs.overlays = [ overlay-unstable ]; })
             ./hosts/mobilerschrott/configuration.nix
+            sops-nix.nixosModules.sops
           ];
           specialArgs = { inherit inputs; };
         };
@@ -32,6 +33,7 @@
           modules = [
             ({ config, pkgs, ...}: { nixpkgs.overlays = [ overlay-unstable ]; })
             ./hosts/matcha-tea-latte/configuration.nix
+            sops-nix.nixosModules.sops
           ];
           specialArgs = { inherit inputs; };
         };
